a140d417fa86ede5a8285c475bf4a427a66aa791
atharv2901ac <atharv2901ac@gmail.com>
2026-06-25 19:07:14 +0530

    26/06/2026

--- THEORY/FILEHANDLING/filehandling04.cpp
+++ THEORY/FILEHANDLING/filehandling04.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <vector>
 using namespace std;
-void EnterDetails(vector <int> Marks){
-    cout << "ENTER THE MARKS OF PHYSICS:";
+void EnterDetails(vector <int>& Marks){
+    Marks.resize(3);
+    cout << "ENTER THE MARKS OF PHYSICS: ";
     cin >> Marks[0];
-    cout << "ENTER THE MARKS OF MATHS:";
+    cout << "ENTER THE MARKS OF MATHS: ";
     cin >> Marks[1];
-    cout << "ENTER THE MARKS OF CHEMISTRY:";
+    cout << "ENTER THE MARKS OF CHEMISTRY: ";
     cin >> Marks[2];
 
     ofstream File("samplemarks.bin", ios::out | ios::binary);
@@ -17,10 +18,27 @@ void EnterDetails(vector <int> Marks){
         File.close();
     }
 }
+void ReadDetails(){
+    vector<int> Marks(3);
+    ifstream File("samplemarks.bin", ios::in | ios::binary);
+
+    if (File.is_open()) {
+        File.read(reinterpret_cast<char*>(Marks.data()), Marks.size() * sizeof(int));
+        File.close();
+
+        cout << "PHYSICS MARKS: " << Marks[0] << endl;
+        cout << "MATHS MARKS: " << Marks[1] << endl;
+        cout << "CHEMISTRY MARKS: " << Marks[2] << endl;
+    } else {
+        cout << "File could not be opened." << endl;
+    }
+
+}
 int main(int argc, char const *argv[])
 {
     vector <int> Marks;
     EnterDetails(Marks);
+    ReadDetails();
     return 0;
 }
 
